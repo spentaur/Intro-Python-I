5053d3dc2322ad8af7abb444f7aee85ae1856308
Spencer Adams <spenceradams@Spencers-MacBook-Pro.local>
2020-07-09 21:24:11 -0500

    final half

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -7,9 +7,12 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
-# Note: pay close attention to your current directory when trying to open "foo.txt"
+# Note: pay close attention to your current directory when trying to open
+# "foo.txt"
 
 # YOUR CODE HERE
+with open("foo.txt", "r") as f:
+    print(f.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +20,13 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open("bar.txt", "w") as f:
+    lines = [
+        "line 1 \n",
+        "line 2 \n",
+        "line 3"
+    ]
+    f.writelines(lines)
+
+with open("bar.txt", "r") as f:
+    print(f.read())
--- src/14_cal.py
+++ src/14_cal.py
@@ -18,15 +18,36 @@ and does the following:
    the format that your program expects arguments to be given.
    Then exit the program.
 
-Note: the user should provide argument input (in the initial call to run the file) and not 
+Note: the user should provide argument input (in the initial call to run the file) and not
 prompted input. Also, the brackets around year are to denote that the argument is
 optional, as this is a common convention in documentation.
 
-This would mean that from the command line you would call `python3 14_cal.py 4 2015` to 
-print out a calendar for April in 2015, but if you omit either the year or both values, 
+This would mean that from the command line you would call `python3 14_cal.py 4 2015` to
+print out a calendar for April in 2015, but if you omit either the year or both values,
 it should use today’s date to get the month and year.
 """
 
 import sys
 import calendar
 from datetime import datetime
+
+
+def main():
+    args = sys.argv
+    month = datetime.now().month
+    year = datetime.now().year
+    if len(args) == 2:
+        month = int(args[1])
+        year = datetime.now().year
+    elif len(args) == 3:
+        month = int(args[1])
+        year = int(args[2])
+    elif len(args) > 3:
+        print("Input should be [month] [year]")
+        return
+
+    print(calendar.month(year, month))
+
+
+if __name__ == "__main__":
+    main()
